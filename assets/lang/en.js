ef9c7cbfdb79454efddabd2f6dfd4d0f0fc20611
Ruggiero Santo <ruggiero.santo@gmail.com>
2021-03-21 10:39:11 +0100

    Fix minor text error

--- assets/lang/en.js
+++ assets/lang/en.js
@@ -5,19 +5,23 @@ en_lang = {
     "ED-UniPi-specialization": "Computer Science - Artificial Intelligence",
     "ED-UniPi": 'All information about the degree can be found on the UniPi <a href="https://didattica.di.unipi.it/en/master-programme-in-computer-science/curricula-2/curriculum-%c2%93artificial-intelligence%c2%94-2/">website</a>. All teaching material and activities were performed in English. During the course of study, in addition to all the exams foreseen for the IA curriculum, I took many exams in the ICT curriculum.</br>List of exams taken:',
     "ED-UniPi-extra": "I took three exams in addition to those required for the degree (<i>peer to peer systems and blockchains</i>, <i>advanced software engineering</i> and <i>ICT risk assessment</i>). Exams that are not on the AI syllabus, but I have taken these for personal interest. Certification provided by the University of Pisa <a href='assets/doc/Laureaconesami.pdf'>here</a>.",
+    "ED-UniPi-period": "Nov 2017 - Mar 2021",
     
     "ED-UniBa-name": "University of Bari",
     "ED-UniBa-title": "Bachelor of Science",
     "ED-UniBa-specialization": "Computer Science and Technologies for Software Production",
     "ED-UniBa": 'All information about the degree can be found on the UniBa <a href="https://www.uniba.it/ricerca/dipartimenti/informatica/didattica/corsi-di-laurea/corsi/informatica-produzione-software/scheda-cd-informatica-produzione-software">website</a>.<br>Most of the exams are non-elective, the only two elective exams taken during the final year are <i>Multimedia Systems</i> and <i>Mobile Development</i>. </br>List of exams taken:',
+    "ED-UniBa-period": "Sep 2014 - Oct 2017",
     
     "ED-sup-title": "Specialist Technician - Computer Science and Telecommunications",
     "ED-sup": 'The five-year course is divided into a two-year course, the same for everyone, in which various non-specialist subjects are studied, <a href="http://www.itisandria.edu.it/index.php/la-scuola/carta-d-identita-della-scuola/il-biennio">see here</a>, and a three-year with various subjects strictly related to the chosen branch of industry <a href="http://www.itisandria.edu.it/index.php/la-scuola/carta-d-identita-della-scuola/l-informatica">see here</a>, in my case in Computer Science and Telecommunications.',
-
+    "ED-sup-period": "Jun 2014",
+    
     "EX-Pi": "Period in which I did my thesis work.",
     "EX-Pi-thesis": "<b>Title</b>: Dynamic neural networks for COVID-19 severity prediction from lung ultrasound. </br> <b>Abstract</b>: The thesis deals with the preparation and predictive analysis of lung ultrasound video to predict the severity of COVID-19 effects. The acquired videos were cleared, organized and precessed for training and benchmarking different deep learning models from action recognition literature. The developed models were also evaluated from a clinical perspective against expert medical opinion. </br> </br> The document is in English and all the work was carried out in complete autonomy in consultation with doctors with expertise in LUS for clinical validation. </br> Soon at least one paper will be published on this work, so more information will be added later.",
-    "EX-Ba": "Period in which I did part of my thesis work.",
+    "EX-Ba": "300-hour internship where I did part of my thesis work",
     "EX-Ba-thesis": "<b>Title</b>: Software system with the application of machine learning algorithms to support the pathological diagnosis of leukocyte cells</br> <b>Abstract</b>: During the thesis work, a software system was created to solve the problem of recognising and classification blood cells, specifically leucocytes, previously extracted from a blood smear image. Machine learning algorithms such as multilevel neural networks and decision trees were applied, implemented, and subsequently compared to solve this problem. The construction of these predictive models was trained and evaluated using a dataset, created during the thesis, of cells labelled with specialised doctors' help to classify as close as possible to clinical reality. The results obtained from both models were then compared and evaluated to obtain a handy product that doctors could use. </br>",
+    "EX-Ba-period": "May 2017 - Jul 2017",
 
     "SF-autonomy": "Autonomy",
     "SF-selfConf": "Self-confidence",
@@ -37,7 +41,7 @@ en_lang = {
     "H-explanation": 'Given my passion for scouting (<a href="https://www.assoraider.it/">my scout organisation</a>), I never miss a chance to do all the activities that allow me to immerse myself in nature. I love to make new experiences and engage in mental and physical challenges that allow to refine all the techniques such as Camping, Pioneering, Trekking, Orienteering and Cooking with a wood fire, in one word Adventure! <br> I am continually discovering and experimenting with new technologies to learn new things and create small software projects that can be useful to me in my daily life or useful to those around me.<br>',
 
     "SK-Python": "Experience: High</br> Daily use in different areas. During the Master's degree course it was used in almost all exams. The two thesis works were carried out with this language.",
-    "SK-Java": "Experience: High</br> First language learnt during high school used in different areas (Created two management programs for an exam and for the family business). Widely used also during Bachelor degree in almost all exams while in Master degree only in some personal project or exams. (Java 8) ",
+    "SK-Java": "Experience: High</br> First language learnt during high school used in different areas (Also with big project, two management programs for an exam and for the family business). Widely used during Bachelor degree in almost all exams while in Master degree only in some personal project or exams. (Java 8) ",
     "SK-C++": "Experience: Medium-High</br> First language in parate with Java during high school, used for some projects in triennial and in magistral in (<a href='https://github.com/Ruggiero-Santo/Parallel-Distributed-Systems'>project</a>).",
     "SK-C": "Experience: Medium-High</br> First language in parsing with Java during high school, used for some three-year projects.",
     "SK-JavaScript": "Experience: Medium</br> Studied in high school and used for several personal projects and in depth for an exam in the Master's degree (<a href='https://github.com/Ruggiero-Santo/DAppAuction'>project</a>).",
--- assets/lang/it.js
+++ assets/lang/it.js
@@ -5,19 +5,23 @@ it_lang = {
     "ED-UniPi-specialization": "Informatica - Intelligenza Artificiale",
     "ED-UniPi": 'Tutte le informazione sul corso e sulla laurea possono essere trovate sul <a href="https://didattica.di.unipi.it/en/master-programme-in-computer-science/curricula-2/curriculum-%c2%93artificial-intelligence%c2%94-2/">sito</a> dell\'UniPi. Tutto il materiale didattico e le lezioni sono state fatta in lingua inglese. Durante il percorso di studio, oltre a tutti gli esami previsti per il curriculum di IA, ho fatto molti esami previsti per il curriculum di ICT. </br> Lista degli esami fatti:',
     "ED-UniPi-extra": "Durente il percoso di studi ho fatto tre esami aggiuntivi rispetto a quelli richiesti per la laurea(<i>sistemi peer to peer e blockchains</i>, <i>ingegneria del software avanzata</i>, <i>valutazione del richio </i>). Questi esami non sono previsti per curriculim di IA, ma sono stati esegiuti per puro interesse personale. Certificazione erogata dell'Università di Pisa <a href='assets\doc\Laureaconesami.pdf'>qui</a>.",
-    
+    "ED-UniPi-period": "Nov 2017 - Mar 2021",    
+
     "ED-UniBa-name": "Università di Bari",
     "ED-UniBa-title": "Bachelor of Science",
     "ED-UniBa-specialization": "Informatica e Tecnologie per la Produzione del Software",
     "ED-UniBa": 'Tutte le informazione sul corso e sulla laurea possono essere trovate sul <a href="https://www.uniba.it/ricerca/dipartimenti/informatica/didattica/corsi-di-laurea/corsi/informatica-produzione-software/scheda-cd-informatica-produzione-software">sito</a> dell\'UniBa. </br> La maggior parte degli esami conseguiti sono obbigatori per l\'indirizzo ITPS, gli unici esami a scelta sono <i>Sistemi Multimediali</i> and <i>Sviluppo di mobile software</i>. </br> Lista degli esami fatti:',
+    "ED-UniBa-period": "Set 2014 - Ott 2017",
 
     "ED-sup-title": "Tecnico Specializzato - Informatica e Telecomunicazioni",
     "ED-sup": 'The five-year course is divided into a two-year course, the same for everyone, in which various non-specialist subjects are studied, <a href="http://www.itisandria.edu.it/index.php/la-scuola/carta-d-identita-della-scuola/il-biennio">see here</a>, and a three-year with various subjects strictly related to the chosen branch of industry <a href="http://www.itisandria.edu.it/index.php/la-scuola/carta-d-identita-della-scuola/l-informatica">see here</a>, in my case in Computer Science and Telecommunications.',
+    "ED-sup-period": "Giu 2014",
 
     "EX-Pi": "Periodo in cui ho fatto il mio lavoro di tesi.",
     "EX-Pi-thesis": "<b>Title</b>: Dynamic neural networks for COVID-19 severity prediction from lung ultrasound. </br> <b>Abstract</b>: The thesis deals with the preparation and predictive analysis of lung ultrasound video to predict the severity of COVID-19 effects. The acquired videos were cleared, organized and precessed for training and benchmarking different deep learning models from action recognition literature. The developed models were also evaluated from a clinical perspective against expert medical opinion. </br> </br> L'elaborato è in inglese e tutto il lavoro è stato svolto in completa autonomia confrontandosi con i dottori esperti in LUS per avere una validazione clinica. </br>A breve almeno un articolo scientifico verrà pubblicato su questo lavoro, quindi presto verranno inserite maggiori informazioni.",
-    "EX-Ba": "Periodo in cui ho fatto parte del mio lavoro di tesi",
+    "EX-Ba": "Tirocinio di 300 ore in cui ho svolto parte del lavoro di tesi",
     "EX-Ba-thesis": "<b>Title</b>: Software system with the application of machine learning algorithms to support the pathological diagnosis of leukocyte cells</br> <b>Abstract</b>: During the thesis work, a software system was created to solve the problem of recognising and classification blood cells, specifically leucocytes, previously extracted from a blood smear image. Machine learning algorithms such as multilevel neural networks and decision trees were applied, implemented, and subsequently compared to solve this problem. The construction of these predictive models was trained and evaluated using a dataset, created during the thesis, of cells labelled with specialised doctors' help to classify as close as possible to clinical reality. The results obtained from both models were then compared and evaluated to obtain a handy product that doctors could use. </br>",
+    "EX-Ba-period": "Mag 2017 - Lug 2017",
 
     "SF-autonomy": "Autonomia",
     "SF-selfConf": "Fiducia in se stessi",
@@ -37,7 +41,7 @@ it_lang = {
     "H-explanation": 'Data la passione per lo scoutismo(<a href="https://www.assoraider.it/">associazione scout</a>), non perdo occasione per fare tutte le attività che mi permettono immergendomi nella natura. Amo fare nuove esperiente e cimentarmi in sfide mentali e fisiche e quindi affinare tutte le tecniche come Campeggio, Pionerismo, Trekking, Orientiring e Cucina con fuoco a legna in ogni loro sfaccettaura, in una parola Avventura! <br> Sono alla continua scoperta e sperimentazione di nuove tecnologie per la creazione di piccoli progetti software che mi possono essere utili durante la vita quotidiata o possono essere utili a chi mi circonda. <!-- Competenze basilari nella lavorazione del legno, installazione di impianti elettrici e idraulici, nozioni di primo soccorso.',
 
     "SK-Python": "Esperienza: Alta</br> Utilizzo quotidiano in diversi ambiti. Duante il percorso magistrale è stato utilizzato in quasi tutti gli esami. I due lavori di tesi sono stati realizzati con questo linguaggio.", 
-    "SK-Java": "Esperienza: Alta</br> Primo linguaggio imparato durante le scuole superiori utilizzato in diversi ambiti (Creati due programmi gestionali per un esame e per l'azienda di famiglia). Largamente utilizzato anche durante la laurea triennale in quasi tutti gli esami mentre in magistrale solo in qualche progetto personale o esami. (Java 8)", 
+    "SK-Java": "Esperienza: Alta</br> Primo linguaggio imparato durante le scuole superiori utilizzato in diversi ambiti (Anche con grossi progetti, due programmi gestionali per un esame e per l'azienda di famiglia). Largamente utilizzato durante la laurea triennale in quasi tutti gli esami mentre in magistrale solo in qualche progetto personale o esami. (Java 8)", 
     "SK-C++": "Esperienza: Medio-Alta</br> Primo linguaggio in parato con Java durante le scuole superiori, utilizzato per alcuni progetti in triennale e in magistrale in (<a href='https://github.com/Ruggiero-Santo/Parallel-Distributed-Systems'>progetto</a>)", 
     "SK-C": "Esperienza: Medio-Alta</br>  Primo linguaggio in parato con Java durante le scuole superiori, utilizzato per alcuni progetti in triennale.", 
     "SK-JavaScript": "Esperienza: Media</br> Studiato alle superiori e utilizzato durante diversi progetti personali e in maniera approfondita in un esame in magistrale (<a href='https://github.com/Ruggiero-Santo/DAppAuction'>progetto</a>)", 
